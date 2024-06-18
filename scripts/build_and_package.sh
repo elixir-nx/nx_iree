6e99e6857eb17b57a1a53a62f10ecd4cad6bcb1b
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2024-06-17 23:40:42 -0300

    chore: better artifact names

--- scripts/build_and_package.sh
+++ scripts/build_and_package.sh
@@ -12,11 +12,12 @@ do
 IREE_CMAKE_BUILD_DIR=iree-runtime/${IREE_BUILD_TARGET}/iree-build
 IREE_RUNTIME_BUILD_DIR=iree-runtime/${IREE_BUILD_TARGET}/build
 IREE_INSTALL_DIR=iree-runtime/${IREE_BUILD_TARGET}/install
+HOST_ARCH=$(uname -s)-$(uname -m)
 
 echo "Building for target: ${IREE_BUILD_TARGET}"
 make ${NUM_JOBS} compile IREE_GIT_REV=$(mix iree.version) IREE_INSTALL_DIR=${IREE_INSTALL_DIR} IREE_CMAKE_BUILD_DIR=${IREE_CMAKE_BUILD_DIR} IREE_RUNTIME_BUILD_DIR=${IREE_RUNTIME_BUILD_DIR} IREE_BUILD_TARGET=${IREE_BUILD_TARGET}
 
-TAR_NAME=iree-runtime/artifacts/iree-runtime-${IREE_BUILD_TARGET}.tar.gz
+TAR_NAME=iree-runtime/artifacts/nx_iree-${HOST_ARCH}-${IREE_BUILD_TARGET}.tar.gz
 
 echo "Packaging into ${TAR_NAME}"
 tar -czf ${TAR_NAME} -C ${IREE_INSTALL_DIR} .
