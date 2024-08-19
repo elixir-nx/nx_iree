89e45f98061eb5906525946e5e88c3368dc412ee
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2024-08-19 09:50:52 -0300

    feat: improve download script

--- lib/mix/tasks/nx_iree/native_download.ex
+++ lib/mix/tasks/nx_iree/native_download.ex
@@ -7,8 +7,8 @@ defmodule Mix.Tasks.NxIree.NativeDownload do
 
   @valid_platforms ~w(host ios ios_simulator visionos visionos_simulator tvos tvos_simulator)
   @platform "--platform <" <> Enum.join(@valid_platforms, "|") <> ">"
-  @destination "--destination <path>"
-  @shortdoc "#{@platform} #{@destination}"
+  @output_dir "--output-dir <path>"
+  @shortdoc "#{@platform} #{@output_dir}"
 
   use Mix.Task
 
@@ -16,12 +16,12 @@ defmodule Mix.Tasks.NxIree.NativeDownload do
   def run(args) do
     {parsed, _rest, _errors} =
       OptionParser.parse(args,
-        switches: [platform: :string, destination: :string],
-        aliases: [p: :platform, d: :destination]
+        switches: [platform: :string, output_dir: :string],
+        aliases: [p: :platform, d: :output_dir]
       )
 
     platform = parsed[:platform]
-    destination = parsed[:destination]
+    output_dir = parsed[:output_dir]
 
     unless platform do
       raise """
@@ -29,14 +29,14 @@ defmodule Mix.Tasks.NxIree.NativeDownload do
       """
     end
 
-    unless destination do
+    unless output_dir do
       raise """
-      Missing required argument: #{@destination}
+      Missing required argument: #{@output_dir}
       """
     end
 
     platform = platform |> String.trim() |> String.downcase()
-    destination = String.trim(destination)
+    output_dir = String.trim(output_dir)
 
     os = :os.type()
     arch = List.to_string(:erlang.system_info(:system_architecture))
@@ -48,19 +48,16 @@ defmodule Mix.Tasks.NxIree.NativeDownload do
 
     file = "nx_iree-embedded-macos-#{platform}.tar.gz"
 
-    downloaded_file =
-      if File.dir?(destination), do: Path.join(destination, file), else: destination
+    File.mkdir_p!(output_dir)
+
+    downloaded_file = Path.join(output_dir, file)
 
     url = NxIREE.MixHelpers.github_release_path(file)
     :ok = NxIREE.MixHelpers.download!("Native NxIREE library (#{platform})", url, downloaded_file)
 
-    parent_dir = Path.dirname(downloaded_file)
-
-    dbg({downloaded_file, parent_dir})
-
     :ok =
       downloaded_file
       |> String.to_charlist()
-      |> :erl_tar.extract([:compressed, cwd: String.to_charlist(parent_dir)])
+      |> :erl_tar.extract([:compressed, cwd: String.to_charlist(output_dir)])
   end
 end
